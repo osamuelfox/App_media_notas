382fd1c72d718ef3d1b9dd954adf488ec014608e
osamuelfox <samuelsantosdesouza92@gmail.com>
2023-07-12 20:29:17 -0300

    Funcionando na main

--- Media de Notas/app/src/main/java/com/samuel/mediadenotas/view/MainActivity.java
+++ Media de Notas/app/src/main/java/com/samuel/mediadenotas/view/MainActivity.java
@@ -12,10 +12,13 @@ import com.samuel.mediadenotas.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText edit_nota1_portugues;
-    EditText edit_nota2_portugues;
-    EditText edit_nota3_portugues;
-    EditText edit_nota4_portugues;
+    EditText edit_nota1_portugues, edit_nota2_portugues, edit_nota3_portugues, edit_nota4_portugues;
+
+    EditText edit_nota1_matematica,edit_nota2_matematica,edit_nota3_matematica,edit_nota4_matematica;
+
+    EditText edit_nota1_geografia, edit_nota2_geografia,edit_nota3_geografia,edit_nota4_geografia;
+
+    EditText edit_nota1_historia, edit_nota2_historia, edit_nota3_historia, edit_nota4_historia;
 
     Button bt_calcular;
     TextView text_resultado;
@@ -25,32 +28,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bt_calcular = findViewById(R.id.bt_calcular);
+        text_resultado = findViewById(R.id.text_resultado);
+
         iniciandoPortugues();
+        iniciandoMatematica();
+        iniciandoGeografia();
+        iniciandoHistoria();
 
         bt_calcular();
     }
-
-
 
     private void bt_calcular() {
         bt_calcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                double nota1 = Double.parseDouble(edit_nota1_portugues.getText().toString());
-                double nota2 = Double.parseDouble(edit_nota2_portugues.getText().toString());
-                double nota3 = Double.parseDouble(edit_nota3_portugues.getText().toString());
-                double nota4 = Double.parseDouble(edit_nota4_portugues.getText().toString());
-
-                double media = CalcularMedia(nota1, nota2, nota3, nota4);
-
-                if (media >= 6){
-                    text_resultado.setText("" + media + " Aluno Aprovado");
-                }
-                else {
-                    text_resultado.setText("" + media + " Aluno Reprovado");
-                }
+                resultado();
             }
         });
+    }
+
+    private void resultado(){
+
+        text_resultado.setText(notasPortugues() + "\n" + notasMatematica() + "\n" + notasGeografia() + "\n" + notasHistoria());
     }
 
     private double CalcularMedia(double nota1, double nota2, double nota3, double nota4) {
@@ -58,12 +58,103 @@ public class MainActivity extends AppCompatActivity {
         return mediaFinal;
     }
 
+    private String notasPortugues() {
+        double nota1 = Double.parseDouble(edit_nota1_portugues.getText().toString());
+        double nota2 = Double.parseDouble(edit_nota2_portugues.getText().toString());
+        double nota3 = Double.parseDouble(edit_nota3_portugues.getText().toString());
+        double nota4 = Double.parseDouble(edit_nota4_portugues.getText().toString());
+
+        double mediaPortugues = CalcularMedia(nota1, nota2, nota3, nota4);
+        String resultado = null;
+
+        if (mediaPortugues >= 6){
+            resultado = ("Portugues " + mediaPortugues + " Aluno Aprovado");
+        }
+        else if (mediaPortugues < 5){
+            resultado = ("Portugues " + mediaPortugues + " Aluno Reprovado");
+        }
+        return resultado;
+    }
+
     private void iniciandoPortugues() {
         edit_nota1_portugues = findViewById(R.id.edit_nota1_portugues);
         edit_nota2_portugues = findViewById(R.id.edit_nota2_portugues);
         edit_nota3_portugues = findViewById(R.id.edit_nota3_portugues);
         edit_nota4_portugues = findViewById(R.id.edit_nota4_portugues);
-        bt_calcular = findViewById(R.id.bt_calcular);
-        text_resultado = findViewById(R.id.text_resultado);
+    }
+
+    private String notasMatematica() {
+        double nota1 = Double.parseDouble(edit_nota1_matematica.getText().toString());
+        double nota2 = Double.parseDouble(edit_nota2_matematica.getText().toString());
+        double nota3 = Double.parseDouble(edit_nota3_matematica.getText().toString());
+        double nota4 = Double.parseDouble(edit_nota4_matematica.getText().toString());
+
+        double mediaMatematica = CalcularMedia(nota1, nota2, nota3, nota4);
+        String resultado = null;
+
+        if (mediaMatematica >= 6){
+            resultado = ("Matemaica " + mediaMatematica + " Aluno Aprovado");
+        }
+        else if (mediaMatematica < 5){
+            resultado = ("Matematica " + mediaMatematica + " Aluno Reprovado");
+        }
+        return resultado;
+    }
+
+    private void iniciandoMatematica() {
+        edit_nota1_matematica = findViewById(R.id.edit_nota1_matematica);
+        edit_nota2_matematica = findViewById(R.id.edit_nota2_matematica);
+        edit_nota3_matematica = findViewById(R.id.edit_nota3_matematica);
+        edit_nota4_matematica = findViewById(R.id.edit_nota4_matematica);
+    }
+
+    private String notasGeografia() {
+        double nota1 = Double.parseDouble(edit_nota1_geografia.getText().toString());
+        double nota2 = Double.parseDouble(edit_nota2_geografia.getText().toString());
+        double nota3 = Double.parseDouble(edit_nota3_geografia.getText().toString());
+        double nota4 = Double.parseDouble(edit_nota4_geografia.getText().toString());
+
+        double mediaGeografia = CalcularMedia(nota1, nota2, nota3, nota4);
+        String resultado = null;
+
+        if (mediaGeografia >= 6){
+            resultado = ("Geografia " + mediaGeografia + " Aluno Aprovado");
+        }
+        else if (mediaGeografia < 5){
+            resultado = ("Geografia " + mediaGeografia + " Aluno Reprovado");
+        }
+        return resultado;
+    }
+
+    private void iniciandoGeografia() {
+        edit_nota1_geografia = findViewById(R.id.edit_nota1_geografia);
+        edit_nota2_geografia = findViewById(R.id.edit_nota2_geografia);
+        edit_nota3_geografia = findViewById(R.id.edit_nota3_geografia);
+        edit_nota4_geografia = findViewById(R.id.edit_nota4_geografia);
+    }
+
+    private String notasHistoria() {
+        double nota1 = Double.parseDouble(edit_nota1_historia.getText().toString());
+        double nota2 = Double.parseDouble(edit_nota2_historia.getText().toString());
+        double nota3 = Double.parseDouble(edit_nota3_historia.getText().toString());
+        double nota4 = Double.parseDouble(edit_nota4_historia.getText().toString());
+
+        double mediaHistoria = CalcularMedia(nota1, nota2, nota3, nota4);
+        String resultado = null;
+
+        if (mediaHistoria >= 6){
+            resultado = ("Historia " + mediaHistoria + " Aluno Aprovado");
+        }
+        else if (mediaHistoria < 5){
+            resultado = ("Historia " + mediaHistoria + " Aluno Reprovado");
+        }
+        return resultado;
+    }
+
+    private void iniciandoHistoria() {
+        edit_nota1_historia = findViewById(R.id.edit_nota1_historia);
+        edit_nota2_historia = findViewById(R.id.edit_nota2_historia);
+        edit_nota3_historia = findViewById(R.id.edit_nota3_historia);
+        edit_nota4_historia = findViewById(R.id.edit_nota4_historia);
     }
 }
